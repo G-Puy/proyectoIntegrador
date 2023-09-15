ed309c337c7a7493fc8fc629034efb72b93fab73
Guillermo P <puppo.estu@gmail.com>
2023-09-14 21:06:17 -0300

    Se logro el scroll, aun falta ocultar la barra y cerrar el overlay

--- src/app/modules/home/Menu/menu.component.ts
+++ src/app/modules/home/Menu/menu.component.ts
@@ -1,3 +1,4 @@
+import { OverlayRef } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,7 +8,8 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  overlayRef!: OverlayRef; // Declara la propiedad overlayRef
 
-
+  constructor() { }
 
 }
