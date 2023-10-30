7bf5f1eca1c78ceeb87bfdcf5898ebcf32ae32a5
Guillermo P <puppo.estu@gmail.com>
2023-10-29 21:17:40 -0300

    FEAT: creando servicios de login

--- src/app/modules/VistaEmpresa/pages/login/login.component.ts
+++ src/app/modules/VistaEmpresa/pages/login/login.component.ts
@@ -8,25 +8,39 @@ import {
   Validators
 } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { LoginService } from './login.service';
+import { User } from 'src/app/interfaces/user.interfce';
+interface LoginForm {
+  usuario: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   formGroupLogin: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private loginService: LoginService) {
     this.formGroupLogin = this.formBuilder.group({
-      usuario: [null, Validators.required],
-      password: [null, Validators.required]
-    })
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    });
 
   }
   ngOnInit() {
 
   }
-
+  /* onLogin(): void {
+    const formGroupLogin = this.formGroupLogin;
+    let usuario: string = formGroupLogin.get('usuario').value;
+    this.loginService.login(this.formGroupLogin.get('usuario').value, this.formGroupLogin.get('password').value)
+      .subscribe(user => { })
+  } */
 
   onSubmit() { }
 }
--- src/app/modules/VistaEmpresa/pages/login/login.service.ts
+++ src/app/modules/VistaEmpresa/pages/login/login.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from 'src/app/interfaces/user.interfce';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private baseURL = '';
+  private user?: User;
+  constructor(private http: HttpClient) {
+  }
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+    return structuredClone(this.user);//Puede llegar a no funcionar por compatibilidad
+  }
 
-  constructor() { }
+  login(usuario: string, password: string): Observable<User> {
+    //http.post('login',{usuario, password});
+    return this.http.get<User>(`${this.baseURL}/users/1`)
+      .pipe(
+        tap(resp => this.user = resp),
+        tap(user => localStorage.setItem('token', user.id.toString()))
+      )
+  }
 }
