f888160d7a856fa58a96f04b3e8a9b70c93ac2bb
Guillermo P <puppo.estu@gmail.com>
2023-12-14 21:47:16 -0300

    card terminada

--- src/app/modules/VistaClientes/pages/DialogUnProducto/DialogUnProducto.component.ts
+++ src/app/modules/VistaClientes/pages/DialogUnProducto/DialogUnProducto.component.ts
@@ -2,6 +2,7 @@ import { Overlay } from '@angular/cdk/overlay';
 import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
+import { DTOImagen } from 'src/app/interfaces/DTOImagen.interface';
 import { recibirProductoDTOBack } from 'src/app/interfaces/DTOsTraerTodosBack/recibirProductoDTOBack.interface';
 import { DTOGenAbms } from 'src/app/interfaces/objGenericoParaABMS.interface';
 import { FuncionesGlobalesService } from 'src/app/shared/funciones-globales.service';
@@ -32,10 +33,34 @@ export class DialogUnProductoComponent {
       this.cargaColores = colores;
     });
     this.objetoProducto = data.producto;
+    for (let index = 0; index < this.data.producto.imagenes.length; index++) {
+      const element = this.data.producto.imagenes[index];
+      this.silderImages.push(this.cargarSrc(element));
+    }
     console.log(this.objetoProducto);
+  }
+  public cargarSrc(img: DTOImagen): string {
+    return `data:image/${img.extension};base64,${img.imagen}`;
   }
   public darSrcProducto(): string {
     return `data:image/${this.objetoProducto!.imagenes[0].extension};base64,${this.objetoProducto!.imagenes[0].imagen}`;
+  }
+  silderImages: string[] = [];
+  iteradorImg: number = 0;
+  iterarIzquierda() {
+    if (this.iteradorImg == 0) {
+      this.iteradorImg = this.silderImages.length - 1;
+    } else {
+      this.iteradorImg--;
+    }
+  }
+  iterarDerecha() {
+    if (this.iteradorImg + 1 == this.silderImages.length) {
+      this.iteradorImg = 0;
+    }
+    else {
+      this.iteradorImg++;
+    }
   }
 
 }
