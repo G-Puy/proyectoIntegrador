632722a39b3aa291665bccb7c22c732758c5ce83
Guillermo P <puppo.estu@gmail.com>
2023-12-20 00:07:02 -0300

    filtros en productos listo, falta que se precargen partiendo de donde s ellamen

--- src/app/modules/VistaClientes/pages/todoslosproductos/todoslosproductos.component.ts
+++ src/app/modules/VistaClientes/pages/todoslosproductos/todoslosproductos.component.ts
@@ -12,9 +12,10 @@ import { SharedService } from 'src/app/shared/shared.service';
   styleUrls: ['./todoslosproductos.component.css']
 })
 export class TodoslosproductosComponent {
+  tipoPrenda: string = '';
   txtBuscar: string = "";
   cargaTiposDePrenda: DTOGenAbms[] = [];
-  opcionBusqueda: string = "Nombre";
+  opcionBusqueda: string = "nombre";
   panelOpenState = false;
   productosFiltrados: recibirProductoDTOBack[] = [];
   todosLosProductos: recibirProductoDTOBack[] = [];
@@ -24,19 +25,53 @@ export class TodoslosproductosComponent {
     private funcionesGlobalesService: FuncionesGlobalesService) {
     this.sharedServ.cargarTodasLosProductos();
     this.todosLosProductos = this.sharedServ.obtenerProductosCargados();
+    this.productosFiltrados = this.todosLosProductos;
+    this.cargarTP();
     //this.traerTodasLosProductos();
   }
   vaciarTxtYBorrarBusqueda() {
-    this.productosFiltrados = [];
+    // this.productosFiltrados = [];
     this.txtBuscar = "";
   }
   filtrar() {
     this.productosFiltrados = [];
-    console.log(this.todosLosProductos);
-    /* if (this.txtBuscar != "") {
-      this.productosFiltrados = this.todosLosProductos.filter(elemento =>
-        elemento.nombre.toLowerCase().includes(this.txtBuscar.toLowerCase())
+    if (this.opcionBusqueda == "nombre") {
+      if (this.txtBuscar != "") {
+        this.productosFiltrados = this.todosLosProductos.filter(elemento =>
+          elemento.nombre.toLowerCase().includes(this.txtBuscar.toLowerCase())
+        );
+      } else {
+        this.productosFiltrados = this.todosLosProductos;
+      }
+    } else if (this.opcionBusqueda == "tipo") {
+      const resultadoFiltradoTipo = this.todosLosProductos.filter(elemento =>
+        elemento.tipoProductoNombre.toLowerCase().includes(this.tipoPrenda.toLowerCase())
       );
-    } */
+      this.productosFiltrados = resultadoFiltradoTipo;
+    } else if (this.opcionBusqueda == "oferta") {
+      const resultadoFiltradoTipo = this.todosLosProductos.filter(elemento =>
+        elemento.precioAnterior > 0
+      );
+      this.productosFiltrados = resultadoFiltradoTipo;
+    } else if (this.opcionBusqueda == "nuevo") {
+      const resultadoFiltradoTipo = this.todosLosProductos.filter(elemento =>
+        elemento.nuevo == true
+      );
+      this.productosFiltrados = resultadoFiltradoTipo;
+    }
   }
+
+  eliminarFiltros() {
+    this.productosFiltrados = this.todosLosProductos;
+  }
+
+
+  //#region CARGA SELECT TIPO PRODUCTO
+  private cargarTP() {
+    this.sharedServ.getAllTipoPrendas().subscribe(tp => {
+      this.cargaTiposDePrenda = tp;
+    });
+
+  }
+  //#endregion
 }
