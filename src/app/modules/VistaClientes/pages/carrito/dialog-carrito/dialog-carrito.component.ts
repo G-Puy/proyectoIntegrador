612a67108de01a89ac641f1105d27c457c623097
Guillermo P <puppo.estu@gmail.com>
2023-12-26 21:39:42 -0300

    casi terminado carrito

--- src/app/modules/VistaClientes/pages/carrito/dialog-carrito/dialog-carrito.component.ts
+++ src/app/modules/VistaClientes/pages/carrito/dialog-carrito/dialog-carrito.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { objCarritoYProcesoDeCompra } from 'src/app/interfaces/DTOsCarritoYProcesoDeCompra/DTOCarritoYProcesoDeCompra.interface';
 import { recibirProductoDTOBack } from 'src/app/interfaces/DTOsTraerTodosBack/recibirProductoDTOBack.interface';
@@ -10,8 +10,9 @@ import { SharedService } from 'src/app/shared/shared.service';
   templateUrl: './dialog-carrito.component.html',
   styleUrls: ['./dialog-carrito.component.css']
 })
-export class DialogCarritoComponent {
+export class DialogCarritoComponent implements OnInit {
   productosDelCarrito: objCarritoYProcesoDeCompra[] = [];
+  total: number = 0;
   constructor(
     private sharedServ: SharedService,
     public dialogRef: MatDialogRef<DialogCarritoComponent>,
@@ -20,8 +21,21 @@ export class DialogCarritoComponent {
     this.productosDelCarrito = sharedServ.obtenerCarrito();
     console.log(this.productosDelCarrito);
   }
+  ngOnInit(): void {
+    this.contarTotal();
+  }
   cargaSrc(producto: objCarritoYProcesoDeCompra) {
     return this.funcionesGlobalesService.cargarSrcCompras(producto);
+  }
+
+  contarTotal() {
+    this.total = 0;
+    this.productosDelCarrito.forEach(producto => {
+      this.total += producto.precio * producto.cantidad;
+    });
+  }
+  onCantidadChange() {
+    this.contarTotal();
   }
 
 }
